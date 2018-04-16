0a3095c878410a644ceaf4f113896dc1f527a2b5
Alexander Bell-Towne <alexander@bell-towne.com>
2018-04-16 10:42:36 -0700

    Update elections page

--- elections/index.php
+++ elections/index.php
@@ -8,12 +8,120 @@
         <h1>Swing Kids Elections</h1>
       </div>
 
+
       <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
-          <div class="list-group">
-            <a href="./2017/" class="list-group-item">2017</a>
-            <a href="./2016/" class="list-group-item">2016</a>
+          <div class="panel panel-default">
+            <div class="panel-heading">Ballots:</div>
+
+            <div class="list-group">
+              <a href="./2017/" class="list-group-item">2017</a>
+              <a href="./2016/" class="list-group-item">2016</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+          <h2>Elected Positions:</h2>
+        </div>
+
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">President</h3>
+              </div>
+              <div class="panel-body">
+                <p>Responsibilities of the President include: scheduling and facilitating officer meetings, delegating tasks to other officers, serving as the main point of contact for the club (for most things), and to an extent, completing tasks assigned to other officers when they are not able to.</p>
+                <h4>Current Officer:</h4>
+                <div class="media">
+                  <img class="pull-left media-object" src="../assets/img/elections/2016/president/diegoChavez.png" alt="Diego Chavez - President">
+                  <div class="media-body">
+                    <h4 class="media-heading">Diego Chavez</h4>
+                    <p>Ask Diego about what it's like to be Swing Kids president</p>
+                  </div>
+                </div>
+
+              </div>
+          </div>
+
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Treasurer</h3>
+              </div>
+              <div class="panel-body">
+                <p>The treasurer is responsible for tracking and overseeing all monetary transactions that involve Swing Kids at UW. This includes, but is not limited to, collecting membership dues, paying instructors, applying for funding, refunding club purchases, and approving or denying proposed uses of SwingKids funds.</p>
+
+                <h4>Current Officer:</h4>
+                <div class="media">
+                  <img class="pull-left media-object" src="../assets/img/elections/2016/treasurer/casiGoodman.jpg" alt="Casi Goodman - Treasurer">
+                  <div class="media-body">
+                    <h4 class="media-heading">Casi Goodman</h4>
+                    <p>Talk to Casi about her role, over the past two years, as Treasurer for Swing Kids.</p>
+                  </div>
+                </div>
+
+              </div>
+          </div>
+
+        </div>
+        <div class="clearfix"></div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Events Coordinators <small>(Two People)</small></h3>
+              </div>
+              <div class="panel-body">
+                <p>The Events Coordinator creates and manages events for the Swing Kids at UW. The responsibilities include booking the bands for the quarter dances, running the afterparty, and creating club bonding activities (such as movie nights, dinners, and get togethers to prepare outfits for the quarter dances).</p>
+
+                <h4>Current Officers:</h4>
+                <div class="media">
+                  <img class="pull-left media-object" src="../assets/img/elections/2016/eventsCoordinator/toriFox.jpg" alt="Tori Fox - Events Coordinator">
+                  <div class="media-body">
+                    <h4 class="media-heading">Tori Fox</h4>
+                    <p>If you have questions about coordinating Swing Kids events, such as our Quarterly Swing Dance, talk to Tori</p>
+                  </div>
+                </div>
+
+                <div class="media">
+                  <img class="pull-left media-object" src="../assets/img/elections/2016/mondayNightDanceCoordinator/sophieKnudson.jpg" alt="Sophie Knudson - Events Coordinator">
+                  <div class="media-body">
+                    <h4 class="media-heading">Sophie Knudson</h4>
+                    <p>Talk to Sophie about our Swing Kids buddy program as well as Swing Kids bonding events.</p>
+                  </div>
+                </div>
+
+              </div>
+          </div>
+
+        </div>
+        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+
+          <div class="panel panel-default">
+              <div class="panel-heading">
+                <h3 class="panel-title">Lessons Coordinator</h3>
+              </div>
+              <div class="panel-body">
+                <p>The Lessons Coordinator books rooms for lessons to be held in each week. Other responsibilities include sending out the weekly email, posting the room locations on the Facebook page and other needed secretarial tasks like taking minutes at officer meetings.</p>
+
+                <h4>Current Officer:</h4>
+                <div class="media">
+                  <img class="pull-left media-object" src="../assets/img/elections/2016/lessonsCoordinator/alexandriaSpofford.jpg" alt="Alexandria Spofford - Lessons Coordinator">
+                  <div class="media-body">
+                    <h4 class="media-heading">Alexandria Spofford</h4>
+                    <p>Ask Alexandria about reserving rooms for every week of lessons and coordinating with The HUB</p>
+                  </div>
+                </div>
+
+              </div>
           </div>
 
         </div>
